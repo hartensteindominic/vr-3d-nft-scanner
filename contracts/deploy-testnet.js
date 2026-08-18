cc51c30a420973ed1db78fc5dd40ea9407ba1995
crypto-mint <hartensteindominic@gmail.com>
2026-08-18 13:12:51 -0400

    fix: deploy HyperStream testnet NFT contract

--- contracts/deploy-testnet.js
+++ contracts/deploy-testnet.js
@@ -2,15 +2,16 @@ const hre = require('hardhat');
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-  console.log('Deploying ThreeNFT with:', deployer.address);
+  console.log('Deploying HyperStreamTestnetNFT with:', deployer.address);
 
-  const Factory = await hre.ethers.getContractFactory('ThreeNFT');
-  const contract = await Factory.deploy('HyperStream 3D', 'H3D');
+  const Factory = await hre.ethers.getContractFactory('HyperStreamTestnetNFT');
+  const contract = await Factory.deploy(deployer.address);
   await contract.waitForDeployment();
 
   const address = await contract.getAddress();
-  console.log('ThreeNFT deployed to:', address);
+  console.log('HyperStreamTestnetNFT deployed to:', address);
   console.log('Owner:', await contract.owner());
+  console.log('Next token ID:', await contract.nextTokenId());
 }
 
 main().catch((error) => {
