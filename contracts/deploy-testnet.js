c981e2c8d4bef10ac798974063e42b2eb437698f
crypto-mint <hartensteindominic@gmail.com>
2026-08-18 12:45:49 -0400

    Deploy ThreeNFT on Sepolia

--- contracts/deploy-testnet.js
+++ contracts/deploy-testnet.js
@@ -1,15 +1,16 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-  console.log("Deploying with:", deployer.address);
+  console.log('Deploying ThreeNFT with:', deployer.address);
 
-  const Factory = await hre.ethers.getContractFactory("HyperStreamTestnetNFT");
-  const contract = await Factory.deploy(deployer.address);
+  const Factory = await hre.ethers.getContractFactory('ThreeNFT');
+  const contract = await Factory.deploy('HyperStream 3D', 'H3D');
   await contract.waitForDeployment();
 
   const address = await contract.getAddress();
-  console.log("HyperStreamTestnetNFT:", address);
+  console.log('ThreeNFT deployed to:', address);
+  console.log('Owner:', await contract.owner());
 }
 
 main().catch((error) => {
